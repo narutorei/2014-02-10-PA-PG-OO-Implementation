f8c7ec9729fdd1a5fb9b3ffc8135e69ec7ba567e
Reinaldo Antonio Camargo Rauch <reinaldorauch@gmail.com>
2014-02-17 14:30:34 -0300

    Criada função que generaliza a exibição dos dados

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,53 +2,39 @@
 #include "pa.cpp"
 #include "pg.cpp"
 
-using namespace std;
+void printSame(PA *pa_pg) {
+	float input = 0.0;
+
+	std::cout << "Escreva os dados para a Progressão:\n";
+
+	std::cout << "Primeiro termo: ";
+	std::cin >> input;
+	pa_pg->setFirstTerm(input);
+	std::cout << '\n';
+
+	std::cout << "Fator: ";
+	std::cin >> input;
+	pa_pg->setRatio(input);
+	std::cout << '\n';
+
+	std::cout << "Número de termos: ";
+	std::cin >> input;
+	pa_pg->setNumTerms(input);
+	std::cout << '\n';
+
+	std::cout << "O " << pa_pg->getNumTerms() << " termo é: " << pa_pg->getNthTerm() << "\n";
+
+	std::cout << "A soma dos termos é " << pa_pg->getSum() << "\n";
+}
 
 int main()
 {
 
 	PA pa1;
 	PG pg1;
-	float input = 0.0;
+	printSame(&pa1);
 
-	std::cout << "Escreva os dados para a PA:\n";
-
-	std::cout << "Primeiro termo: ";
-	std::cin >> input;
-	pa1.setFirstTerm(input);
-	std::cout << '\n';
-
-	std::cout << "Fator: ";
-	std::cin >> input;
-	pa1.setRatio(input);
-	std::cout << '\n';
-
-	std::cout << "Número de termos: ";
-	std::cin >> input;
-	pa1.setNumTerms(input);
-	std::cout << '\n';
-
-	std::cout << "O " << pa1.getNumTerms() << " termo é: " << pa1.getNthTerm() << "\n";
-
-
-	std::cout << "Escreva os dados para a PG:\n";
-
-	std::cout << "Primeiro termo: ";
-	std::cin >> input;
-	pg1.setFirstTerm(input);
-	std::cout << '\n';
-
-	std::cout << "Fator: ";
-	std::cin >> input;
-	pg1.setRatio(input);
-	std::cout << '\n';
-
-	std::cout << "Número de termos: ";
-	std::cin >> input;
-	pg1.setNumTerms(input);
-	std::cout << '\n';
-
-	std::cout << "O " << pg1.getNumTerms() << " termo é: " << pg1.getNthTerm() << "\n";
+	printSame(&pg1);
 
 	return 0;
 }
